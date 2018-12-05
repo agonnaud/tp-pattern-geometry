d4e901dfdfc00418283cf9c1e20b96e8d06ac244
MBorne <mborne@users.noreply.github.com>
2018-12-05 14:33:43 +0100

    0.1 - correction

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,33 @@
 package org.acme.geometry;
 
+/**
+ * 
+ * Classe coordonnée à l'étape 0.1
+ * 
+ * @author MBorne
+ *
+ */
 public class Coordinate {
+	
+	private double x;
+	
+	private double y;
+	
+	public Coordinate() {
+		this(0.0,0.0);
+	}
+	
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
 
 }
--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -8,11 +8,10 @@ public class CoordinateTest {
 	public static final double EPSILON = 1.0e-15;
 	
 	@Test
-	public void testDefaultConstructor(){
-		// TODO
-		//Coordinate c = new Coordinate();
-		//Assert.assertEquals(0.0, c.getX(), EPSILON);
-		//Assert.assertEquals(0.0, c.getY(), EPSILON);
+	public void testDefaultConstructorBehavior(){
+		Coordinate c = new Coordinate();
+		Assert.assertEquals(0.0, c.getX(), EPSILON);
+		Assert.assertEquals(0.0, c.getY(), EPSILON);
 	}
 
 }
